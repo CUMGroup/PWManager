5b1195ea4d6f6942ae8edd3e37b93ec223f8241a
jas20202 <99250573+jas20202@users.noreply.github.com>
2023-10-27 17:09:52 +0200

    updated user

--- PWManager.Domain/Entities/User.cs
+++ PWManager.Domain/Entities/User.cs
@@ -1,15 +1,13 @@
 ﻿using PWManager.Domain.Common;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PWManager.Domain.Entities {
     public class User : Entity {
         public string MasterPassword { get; init; }
-        public User(string masterHash)
-        {
+        public User(string masterHash) : base(){
+            MasterPassword = masterHash;
+        }
+
+        public User(string id, DateTimeOffset created, DateTimeOffset updated, string masterHash) : base(id, created, updated){
             MasterPassword = masterHash;
         }
     }
