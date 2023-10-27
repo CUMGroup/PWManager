979baa27ef3a12e4bc61f43fd1a906080ded7269
jas20202 <99250573+jas20202@users.noreply.github.com>
2023-10-27 17:01:03 +0200

    added constructors

--- PWManager.Domain/Common/Entity.cs
+++ PWManager.Domain/Common/Entity.cs
@@ -6,5 +6,16 @@
         public DateTimeOffset Created { get; init; }
         public DateTimeOffset Updated { get; set; }
 
+        public Entity() {
+            Id = Guid.NewGuid().ToString();
+            Created = DateTimeOffset.Now;
+            Updated = DateTimeOffset.Now;
+        }
+
+        public Entity(string id, DateTimeOffset created, DateTimeOffset updated) {
+            Id = id;
+            Created = created;
+            Updated = updated;
+        }
     }
 }
