043b3b5c37a526c8b19e26b2d6cb05d5f2a7fe51
jas20202 <99250573+jas20202@users.noreply.github.com>
2023-10-27 17:51:31 +0200

    added value object Password

--- PWManager.Domain/Entities/Account.cs
+++ PWManager.Domain/Entities/Account.cs
@@ -1,16 +1,17 @@
 ﻿using PWManager.Domain.Common;
+using PWManager.Domain.ValueObjects;
 
 namespace PWManager.Domain.Entities {
     public class Account : Entity {
         public string LoginName { get; set; }
-        public string Password { get; set; }
+        public Password Password { get; set; }
     
-        public Account(string loginName, string password) : base() { 
+        public Account(string loginName, Password password) : base() { 
             LoginName = loginName;
             Password = password;
         }
 
-        public Account(string id, DateTimeOffset created, DateTimeOffset updated, string loginName, string password) : base(id, created, updated) {
+        public Account(string id, DateTimeOffset created, DateTimeOffset updated, string loginName, Password password) : base(id, created, updated) {
             LoginName = loginName;
             Password = password;
         }
---
+++ PWManager.Domain/ValueObjects/Password.cs
@@ -0,0 +1,22 @@
+﻿
+
+using PWManager.Domain.Common;
+
+namespace PWManager.Domain.ValueObjects {
+    public class Password : ValueObject {
+
+        private readonly string _passwordHash;
+        public string PasswordHash { get => _passwordHash; set => new Password(value, Strength); }
+        private readonly uint _strength;
+        public uint Strength { get => _strength; set => new Password(PasswordHash, value); }
+        public Password(string passwordHash, uint strength) {
+            _passwordHash = passwordHash;
+            _strength = strength;
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents() {
+            yield return PasswordHash;
+            yield return Strength;
+        }
+    }
+}
