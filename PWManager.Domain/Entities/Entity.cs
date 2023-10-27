d6f81bb28fd7478f6cc1238306e901cb97eeb20c
jas20202 <99250573+jas20202@users.noreply.github.com>
2023-10-27 16:35:15 +0200

    created base entity

---
+++ PWManager.Domain/Entities/Entity.cs
@@ -0,0 +1,8 @@
+﻿namespace PWManager.Domain.Entities {
+    public abstract class Entity {
+        public string Id { get; init; }
+        DateTimeOffset Created { get; init; }
+        DateTimeOffset Updated { get; set; }
+        
+    }
+}
