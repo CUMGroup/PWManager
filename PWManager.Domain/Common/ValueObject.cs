8c4307a49e0939e96d7f290708a70bed9eb6b93f
jas20202 <99250573+jas20202@users.noreply.github.com>
2023-10-27 17:38:10 +0200

    added base value object

---
+++ PWManager.Domain/Common/ValueObject.cs
@@ -0,0 +1,21 @@
+﻿
+namespace PWManager.Domain.Common {
+    public abstract class ValueObject {
+        protected abstract IEnumerable<object> GetEqualityComponents();
+
+        public override bool Equals(object? obj) {
+            if (obj is null || obj.GetType() != this.GetType()) {
+                return false;
+            }
+
+            var otherObj = (ValueObject)obj; 
+            return GetEqualityComponents().SequenceEqual(otherObj.GetEqualityComponents());
+        }
+
+        public override int GetHashCode() {
+            return GetEqualityComponents()
+                .Select(x => x is not null ? x.GetHashCode() : 0 )
+                .Aggregate((x, y) => x ^ y);
+        }
+    }
+}
