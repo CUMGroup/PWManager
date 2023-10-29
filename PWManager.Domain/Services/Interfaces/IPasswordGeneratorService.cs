bc9cf9e84d7869d3aa673037c8ba12ba709e797f
catwithamask <121181686+catwithamask@users.noreply.github.com>
2023-10-29 19:04:26 +0100

    crested interface for the domain PasswordGeneratorService

---
+++ PWManager.Domain/Services/Interfaces/IPasswordGeneratorService.cs
@@ -0,0 +1,8 @@
+﻿using PWManager.Domain.ValueObjects;
+
+namespace PWManager.Domain.Services.Interfaces {
+    public interface IPasswordGeneratorService {
+        public string GeneratePasswordWith(PasswordGeneratorCriteria criteria);
+        public string GeneratePassword();
+    }
+}
