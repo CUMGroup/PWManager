37673715dbd3fc1285ba8ac42c10d2509ad350bb
catwithamask <121181686+catwithamask@users.noreply.github.com>
2023-10-29 18:45:37 +0100

    added interface for UserRepository

---
+++ PWManager.Domain/Repositories/IUserRepository.cs
@@ -0,0 +1,8 @@
+﻿using PWManager.Domain.Entities;
+
+namespace PWManager.Domain.Repositories {
+    internal interface IUserRepository {
+        public bool AddUser(User user);
+        public bool CheckPasswordAttempt(string passwordHash);
+    }
+}
