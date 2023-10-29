b94147eb129f47a1d919b6fc6e1a26428dde7658
catwithamask <121181686+catwithamask@users.noreply.github.com>
2023-10-29 18:58:30 +0100

    crested interface for the domain CryptService

---
+++ PWManager.Domain/Services/Interfaces/ICryptService.cs
@@ -0,0 +1,9 @@
+﻿namespace PWManager.Domain.Services.Interfaces
+{
+    public interface ICryptService
+    {
+        public string Encrypt(string input);
+        public string Decrypt(string input);
+        public string Hash(string input);
+    }
+}
