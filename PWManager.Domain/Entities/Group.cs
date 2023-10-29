af7877021194e14e50735606c6bc2741da3a477f
Alex Hagl <alex@hagl.de>
2023-10-29 18:17:29 +0100

    Added methods to hold law of demeter

--- PWManager.Domain/Entities/Group.cs
+++ PWManager.Domain/Entities/Group.cs
@@ -2,20 +2,42 @@
 
 namespace PWManager.Domain.Entities {
     public class Group : Entity {
-        public string UserID { get; set; }
+        public string UserId { get; set; }
         public List<Account> Accounts { get; set; }
 
-        public Group(string userID) : this(userID, new List<Account>()) {}
-        public Group(string id, DateTimeOffset created, DateTimeOffset updated, string userID) : this(id, created, updated, userID, new List<Account>()) {}
+        public Group(string userId) : this(userId, new List<Account>()) {}
+        public Group(string id, DateTimeOffset created, DateTimeOffset updated, string userId) : this(id, created, updated, userId, new List<Account>()) {}
 
-        public Group(string userID, List<Account> accounts) : base() {
-            UserID = userID;
+        public Group(string userId, List<Account> accounts) : base() {
+            UserId = userId;
             Accounts = accounts;
         }
 
-        public Group(string id, DateTimeOffset created, DateTimeOffset updated, string userID, List<Account> accounts) : base(id, created, updated) {
-            UserID = userID;
+        public Group(string id, DateTimeOffset created, DateTimeOffset updated, string userId, List<Account> accounts) : base(id, created, updated) {
+            UserId = userId;
             Accounts = accounts;
+        }
+
+        public void AddAccount(Account acc) {
+            Accounts.Add(acc);
+        }
+
+        public Account? GetAccount(string accId) {
+            return Accounts.Find(e => e.Id.Equals(accId));
+        }
+
+        public bool RemoveAccount(string accId) {
+            var acc = GetAccount(accId);
+            return acc is not null && Accounts.Remove(acc);
+        }
+
+        public bool UpdateAccount(Account acc) {
+            var index = Accounts.FindIndex(e => e.Id.Equals(acc.Id));
+            if (index < 0) {
+                return false;
+            }
+            Accounts[index] = acc;
+            return true;
         }
     }
 }
