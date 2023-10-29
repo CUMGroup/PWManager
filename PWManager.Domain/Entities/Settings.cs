f578faaf4b60157aa40787a35087f5d5ea550f05
Alex Hagl <alex@hagl.de>
2023-10-29 18:19:23 +0100

    Name refactor

--- PWManager.Domain/Entities/Settings.cs
+++ PWManager.Domain/Entities/Settings.cs
@@ -4,20 +4,20 @@ using PWManager.Domain.ValueObjects;
 
 namespace PWManager.Domain.Entities {
     public class Settings : Entity {
-        public string UserID { get; set; }
-        public PasswordGeneratorCriteria PWGenCriteria { get; set; }
+        public string UserId { get; set; }
+        public PasswordGeneratorCriteria PwGenCriteria { get; set; }
         public ClipboardTimeoutSetting ClipboardTimeout { get; set; }
         public MainGroupSetting MainGroup { get; set; }
 
         public Settings(string userId, PasswordGeneratorCriteria pwGenCriteria, ClipboardTimeoutSetting clipboardTimeout, MainGroupSetting mainGroup) : base(){
-            UserID = userId;
-            PWGenCriteria = pwGenCriteria;
+            UserId = userId;
+            PwGenCriteria = pwGenCriteria;
             ClipboardTimeout = clipboardTimeout;
             MainGroup = mainGroup;
         }
         public Settings(string id, DateTimeOffset created, DateTimeOffset updated, string userId, PasswordGeneratorCriteria pwGenCriteria, ClipboardTimeoutSetting clipboardTimeout, MainGroupSetting mainGroup) : base(id, created, updated) {
-            UserID = userId;
-            PWGenCriteria = pwGenCriteria;
+            UserId = userId;
+            PwGenCriteria = pwGenCriteria;
             ClipboardTimeout = clipboardTimeout;
             MainGroup = mainGroup;
         }
