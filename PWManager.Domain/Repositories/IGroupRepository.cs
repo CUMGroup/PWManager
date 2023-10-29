f47958680516750a0f020788c90f3dfb2a58e973
catwithamask <121181686+catwithamask@users.noreply.github.com>
2023-10-29 18:39:32 +0100

    added interface for GroupRepository

---
+++ PWManager.Domain/Repositories/IGroupRepository.cs
@@ -0,0 +1,11 @@
+﻿using PWManager.Domain.Entities;
+
+namespace PWManager.Domain.Repositories {
+    internal interface IGroupRepository {
+        public Group GetGroup(string groupName);
+        public List<string> GetAllGroupNames();
+        public bool AddGroup(Group group);
+        public void UpdateGroup(Group group);
+        public bool RemoveGroup(string groupName);
+    }
+}
