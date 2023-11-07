f633c93f04fb834eb917633a34c59ce6973d5864
Alex <alex@hagl.de>
2023-11-07 11:44:21 +0100

    Added UserSettings Accessor interface

---
+++ PWManager.Application/Interfaces/IUserSettings.cs
@@ -0,0 +1,10 @@
+﻿using PWManager.Domain.ValueObjects;
+
+namespace PWManager.Application.Interfaces; 
+
+public interface IUserSettings {
+
+    public PasswordGeneratorCriteria GetPasswordGeneratorCriteria();
+    public ClipboardTimeoutSetting GetClipboardTimeoutSetting();
+    public MainGroupSetting GetMainGroupSetting();
+}
