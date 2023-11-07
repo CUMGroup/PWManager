44706c09dd9479fffef81e5e57416a65e67f6b1c
Alex <alex@hagl.de>
2023-11-07 11:44:39 +0100

    Added password generator exception type

---
+++ PWManager.Domain/Exceptions/PasswordGenerationException.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.Serialization;
+
+namespace PWManager.Domain.Exceptions; 
+
+public class PasswordGenerationException : Exception {
+    public PasswordGenerationException() { }
+    protected PasswordGenerationException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    public PasswordGenerationException(string? message) : base(message) { }
+    public PasswordGenerationException(string? message, Exception? innerException) : base(message, innerException) { }
+}
