83add0703e7ee0af94769449380fb87880905255
jas20202 <99250573+jas20202@users.noreply.github.com>
2023-11-27 15:07:46 +0100

    finished unit Tests

--- PWManager.UnitTests/Application/CryptServiceTest.cs
+++ PWManager.UnitTests/Application/CryptServiceTest.cs
@@ -7,33 +7,49 @@ namespace PWManager.UnitTests.Application {
 
         [Fact]
         public void CryptService_Should_HashTheSamePassordIdentical() {
-            var testPassword = Encoding.ASCII.GetBytes("password");
+            var testPassword = "password";
+            var testSalt = "salt";
 
             var aes = Aes.Create();
             aes.GenerateIV();
             var sut = new CryptService(aes.IV);
 
-            var hash1 = sut.HashForLogin(testPassword);
-            var hash2 = sut.HashForLogin(testPassword);
+            var hash1 = sut.HashForLogin(testPassword, testSalt);
+            var hash2 = sut.HashForLogin(testPassword, testSalt);
 
             Assert.Equal(hash1, hash2);
         }
 
         [Fact]
-        public void CryptService_Should_EncryptAndDecryptToGetTheMessageBack() {
-            var testPassword = Encoding.ASCII.GetBytes("password");
-            var testSalt = Encoding.ASCII.GetBytes("salt");
-            var testPlain = Encoding.ASCII.GetBytes("Secret Message");
+        public void HashForLogin_ShouldNot_ReturnTheSameAs_DeriveKeyFrom() {
+            var testPassword = "password";
+            var testSalt = "salt";
 
             var aes = Aes.Create();
             aes.GenerateIV();
             var sut = new CryptService(aes.IV);
 
-            var key = sut.HashKey(testPassword, testSalt);
-            var cipher = sut.Encrypt(testPlain, Encoding.ASCII.GetBytes(key));
-            var plain = sut.Decrypt(Encoding.ASCII.GetBytes(cipher), Encoding.ASCII.GetBytes(key));
+            var hash1 = sut.HashForLogin(testPassword, testSalt);
+            var key = sut.DeriveKeyFrom(testPassword, testSalt);
 
-            Assert.Equal(testPlain.ToString(), plain);
+            Assert.NotEqual(hash1, key);
+        }
+
+        [Fact]
+        public void CryptService_Should_EncryptAndDecryptToGetTheMessageBack() {
+            var testPassword = "password";
+            var testSalt = "salt";
+            var testPlain = "Secret Message";
+
+            var aes = Aes.Create();
+            aes.GenerateIV();
+            var sut = new CryptService(aes.IV);
+
+            var key = sut.DeriveKeyFrom(testPassword, testSalt);
+            var cipher = sut.Encrypt(testPlain, key);
+            var plain = sut.Decrypt(cipher, key);
+
+            Assert.Equal(testPlain, plain);
         }
 
     }
