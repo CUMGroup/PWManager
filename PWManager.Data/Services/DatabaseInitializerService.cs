3bcd9bfa5009710c4a6112e8d3df1ec858f4f4de
Alex <alex@hagl.de>
2024-02-24 16:06:12 +0100

    Database Init Tests

--- PWManager.Data/Services/DatabaseInitializerService.cs
+++ PWManager.Data/Services/DatabaseInitializerService.cs
@@ -2,6 +2,7 @@
 using PWManager.Application.Context;
 using PWManager.Application.Exceptions;
 using PWManager.Application.Services.Interfaces;
+using PWManager.Data.Abstraction;
 using PWManager.Domain.Repositories;
 using PWManager.Domain.Services.Interfaces;
 using Group = PWManager.Domain.Entities.Group;
@@ -14,16 +15,23 @@ internal class DatabaseInitializerService : IDatabaseInitializerService {
     private readonly IGroupRepository _groupRepository;
     private readonly IApplicationEnvironment _environment;
     private readonly ICryptService _cryptService;
-    
+    private readonly DataContextWrapper _dataContext;
+
+    internal DatabaseInitializerService(DataContextWrapper wrapper, IUserRepository userRepo,
+        IGroupRepository groupRepository, IApplicationEnvironment environment, ICryptService cryptService) : this(
+        userRepo, groupRepository, environment, cryptService) {
+        _dataContext = wrapper;
+    }
     public DatabaseInitializerService(IUserRepository userRepo, IGroupRepository groupRepository, IApplicationEnvironment environment, ICryptService cryptService) {
         _userRepo = userRepo;
         _groupRepository = groupRepository;
         _environment = environment;
         _cryptService = cryptService;
+        _dataContext = new DataContextWrapper();
     }
     
     public void InitDatabase(string path, string username, string password) {
-        if (DataContext.DatabaseExists(path)) {
+        if (_dataContext.DatabaseExists(path)) {
             throw new UserFeedbackException(
                 "Database initialization failed! The database already exists at the specified path!");
         }
@@ -32,7 +40,7 @@ internal class DatabaseInitializerService : IDatabaseInitializerService {
             throw new UserFeedbackException("Invalid Username! Only letters are allowed!");
         }
         
-        DataContext.InitDataContext(path);
+        _dataContext.InitDataContext(path);
         
         var user = _userRepo.AddUser(username, password);
         _environment.CurrentUser = user;
---
+++ PWManager.UnitTests/Services/DatabaseInitServiceTest.cs
@@ -0,0 +1,56 @@
+﻿using System.Runtime.InteropServices.JavaScript;
+using NSubstitute;
+using PWManager.Application.Context;
+using PWManager.Application.Exceptions;
+using PWManager.Data.Abstraction;
+using PWManager.Data.Services;
+using PWManager.Domain.Entities;
+using PWManager.Domain.Repositories;
+using PWManager.Domain.Services.Interfaces;
+
+namespace PWManager.UnitTests.Services; 
+
+public class DatabaseInitServiceTest {
+
+    private DatabaseInitializerService _sut;
+    private IGroupRepository _groupRepo = Substitute.For<IGroupRepository>();
+    private ICryptService _cryptService = Substitute.For<ICryptService>();
+    private IApplicationEnvironment _env = Substitute.For<IApplicationEnvironment>();
+    private DataContextWrapper _wrapper = Substitute.For<DataContextWrapper>();
+    private IUserRepository _userRepo = Substitute.For<IUserRepository>();
+    
+    [Fact]
+    public void Init_Should_SetEnvironment() {
+        _wrapper.DatabaseExists(Arg.Any<string>()).Returns(false);
+        var user = new User(Guid.NewGuid().ToString(), DateTimeOffset.Now, DateTimeOffset.Now, "TestUserName");
+        _userRepo.AddUser(Arg.Any<string>(), Arg.Any<string>()).Returns(user);
+        
+        
+        _sut = new DatabaseInitializerService(_wrapper,_userRepo,_groupRepo,_env, _cryptService);
+        
+        _sut.InitDatabase(".", "TestUserName", "WhatAPassword");
+        
+        Assert.Equal(user,_env.CurrentUser);
+        Assert.NotNull(_env.EncryptionKey);
+    }
+
+    [Fact]
+    public void Init_ShouldNot_AcceptInvalidName() {
+        _wrapper.DatabaseExists(Arg.Any<string>()).Returns(false);
+        _sut = new DatabaseInitializerService(_wrapper,_userRepo,_groupRepo,_env, _cryptService);
+
+        var ex = Assert.Throws<UserFeedbackException>(() => _sut.InitDatabase(".","asd$", "WhatAPassword"));
+        
+        Assert.Equal("Invalid Username! Only letters are allowed!", ex.Message);
+    }
+    
+    [Fact]
+    public void Init_ShouldNot_InitExistingDb() {
+        _wrapper.DatabaseExists(Arg.Any<string>()).Returns(true);
+        _sut = new DatabaseInitializerService(_wrapper,_userRepo,_groupRepo,_env, _cryptService);
+
+        var ex = Assert.Throws<UserFeedbackException>(() => _sut.InitDatabase(".","TestUserName", "WhatAPassword"));
+        
+        Assert.Equal("Database initialization failed! The database already exists at the specified path!", ex.Message);
+    }
+}
