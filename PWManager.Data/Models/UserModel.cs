ac4d41cae2f330a19df09384ce4758007e08054f
Alex <alex@hagl.de>
2024-02-24 10:52:32 +0100

    Added data context models

---
+++ PWManager.Data/Models/UserModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PWManager.Data.Models; 
+
+internal class UserModel {
+    
+    [Key]
+    public string Id { get; set; }
+    
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public DateTimeOffset Created { get; set; }
+    
+    [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+    public DateTimeOffset Updated { get; set; }
+    
+    [Required]
+    public string MasterHash { get; set; }
+    
+    [Required]
+    public string Salt { get; set; }
+}
