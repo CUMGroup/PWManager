058d840e9ae8ee2e972233c40989a2c49eb7419e
Alex <alex@hagl.de>
2024-02-24 11:37:21 +0100

    Changed cryptservice to use environment key

--- PWManager.Domain/Services/Interfaces/ICryptService.cs
+++ PWManager.Domain/Services/Interfaces/ICryptService.cs
@@ -1,8 +1,10 @@
 ﻿namespace PWManager.Domain.Services.Interfaces {
     public interface ICryptService {
-        public string Encrypt(string input, string key);
-        public string Decrypt(string input, string key);
+        public string Encrypt(string input);
+        public string Decrypt(string input);
         public string Hash(string input, string salt);
         public string DeriveKeyFrom(string input, string salt);
+
+        public string GenerateSalt();
     }
 }
