cdfde46974a20d9f46103dea6ff301e7b3c5141a
jas20202 <99250573+jas20202@users.noreply.github.com>
2024-03-02 19:37:09 +0100

    added groupservice class. Not fully implemented yet

--- PWManager.Application/Services/GroupService.cs
+++ PWManager.Application/Services/GroupService.cs
@@ -1,3 +1,16 @@
-﻿namespace PWManager.Application.Services;
-public class GroupService {
+﻿using PWManager.Application.Services.Interfaces;
+
+namespace PWManager.Application.Services;
+public class GroupService : IGroupService {
+    public void AddGroup(string userID, string identifier) {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteGroup(string identifier) {
+        throw new NotImplementedException();
+    }
+
+    public List<string> GetAllGroupNames() {
+        throw new NotImplementedException();
+    }
 }
