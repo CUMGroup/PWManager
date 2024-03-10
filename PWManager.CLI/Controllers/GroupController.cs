8a7efd7a26139248ce002dfd8dce0d6266f7bdb8
jas20202 <99250573+jas20202@users.noreply.github.com>
2024-03-10 17:10:00 +0100

    now the password will be checked

--- PWManager.CLI/Controllers/GroupController.cs
+++ PWManager.CLI/Controllers/GroupController.cs
@@ -1,9 +1,11 @@
 ﻿using PWManager.Application.Context;
+using PWManager.Application.Services;
 using PWManager.Application.Services.Interfaces;
 using PWManager.CLI.Abstractions;
 using PWManager.CLI.Attributes;
 using PWManager.CLI.Enums;
 using PWManager.CLI.Interfaces;
+using PWManager.Data.Services;
 using Sharprompt;
 
 namespace PWManager.CLI.Controllers;
@@ -11,16 +13,19 @@ namespace PWManager.CLI.Controllers;
 [SessionOnly]
 public class GroupController : IController {
     private readonly IGroupService _groupService;
-    private readonly IUserEnvironment _userEnv; 
+    private readonly IUserEnvironment _userEnv;
+
+    private readonly ILoginService _loginService;
 
     private readonly string newGroup = "New Group";
     private readonly string switchGroup = "Switch Group";
     private readonly string listAllGroups = "List all Groups";
     private readonly string deleteGroup;
 
-    public GroupController(IGroupService groupService, IUserEnvironment userEnv) {
+    public GroupController(IGroupService groupService, IUserEnvironment userEnv, ILoginService loginService) {
         _groupService = groupService;
         _userEnv = userEnv;
+        _loginService = loginService;
         deleteGroup = $"Delete Group '{_userEnv.CurrentGroup!.Identifier}'";
     }
 
@@ -45,21 +50,15 @@ public class GroupController : IController {
         }
 
         if (option.Equals(deleteGroup)) {
-            var group = _userEnv.CurrentGroup!.Identifier;
+            var currentgroup = _userEnv.CurrentGroup!.Identifier;
 
-            var confirm = Prompt.Confirm($"Are you sure you want to delete the group '{group}'");
+            var confirm = Prompt.Confirm($"Are you sure you want to delete the group '{currentgroup}'");
             
             if(!confirm) {
                 return ExitCondition.CONTINUE;
             }
 
-            var pass = Prompt.Password("Enter your password to confirm");
-            // TODO: validate password
-            // if (password not valid) {
-            //  return ExitCondition.CONTINUE;
-            // }
-
-            _groupService.DeleteGroup(group);
+            HandleDeletion(currentgroup);
 
             if (_groupService.GetAllGroupNames().Count > 0) {
                 SwitchGroup();
@@ -83,6 +82,16 @@ public class GroupController : IController {
 
         _groupService.AddGroup(_userEnv.CurrentUser!.Id, groupIdentifier);
         _groupService.SwitchGroup(groupIdentifier);
+    }
+
+    private bool HandleDeletion(string identifier) {
+        if (!PromptHelper.ConfirmDeletion(identifier, _loginService, _userEnv)) {
+            PromptHelper.PrintColoredText(ConsoleColor.Red, "Delete aborted!");
+            return false;
+        }
+        _groupService.DeleteGroup(identifier);
+        PromptHelper.PrintColoredText(ConsoleColor.Green, "Group deleted!");
+        return true;
     }
 }
 
