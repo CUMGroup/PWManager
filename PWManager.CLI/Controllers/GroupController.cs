9f52f58a85e7bb6c8a2bead737adba7ebf4aa9e6
jas20202 <99250573+jas20202@users.noreply.github.com>
2024-03-10 13:35:22 +0100

    eh

--- PWManager.CLI/Controllers/GroupController.cs
+++ PWManager.CLI/Controllers/GroupController.cs
@@ -1,4 +1,28 @@
-﻿namespace PWManager.CLI.Controllers;
-public class GroupController {
+﻿using PWManager.Application.Context;
+using PWManager.Application.Services.Interfaces;
+using PWManager.CLI.Attributes;
+using PWManager.CLI.Enums;
+using PWManager.CLI.Interfaces;
+using Sharprompt;
+
+namespace PWManager.CLI.Controllers;
+
+[SessionOnly]
+public class GroupController : IController {
+    private readonly IGroupService _groupService;
+    private readonly IUserEnvironment _userEnv; 
+    private readonly ICliEnvironment _cliEnv;
+
+    public GroupController(IGroupService groupService, IUserEnvironment userEnv, ICliEnvironment cliEnv) {
+        _groupService = groupService;
+        _userEnv = userEnv;
+        _cliEnv = cliEnv;
+    }
+
+    public ExitCondition Handle(string[] args) {
+        Prompt.Select("Please select", );
+
+        return ExitCondition.CONTINUE;
+    }
 }
 
