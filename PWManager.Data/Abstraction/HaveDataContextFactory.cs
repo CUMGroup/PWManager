6086e8c0a50cd043ac039f9a2460c0e26c6424a6
Alex <alex@hagl.de>
2024-03-23 13:17:29 +0100

    Added Abstractions for data context

---
+++ PWManager.Data/Abstraction/HaveDataContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using PWManager.Data.Abstraction.Interfaces;
+
+namespace PWManager.Data.Abstraction; 
+
+internal static class HaveDataContextFactory {
+
+    private static IHaveDataContext? _dataContext;
+
+    internal static IHaveDataContext Create() {
+        if (_dataContext is null) {
+            throw new ApplicationException("Tried accessing the data context without initializing it first!");
+        }
+        return _dataContext;
+    }
+
+    internal static void Initialize(IHaveDataContext context) {
+        if (_dataContext is not null) {
+            throw new ApplicationException(
+                "The data context was already set. The initialize method can only be called once!");
+        }
+        _dataContext = context;
+    }
+}
---
+++ PWManager.Data/Abstraction/Interfaces/IHaveDataContext.cs
@@ -0,0 +1,7 @@
+﻿using PWManager.Data.Persistance;
+
+namespace PWManager.Data.Abstraction.Interfaces; 
+
+internal interface IHaveDataContext {
+    ApplicationDbContext GetDbContext();
+}
