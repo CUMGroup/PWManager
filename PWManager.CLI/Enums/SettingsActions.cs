a04ff63f050d41cd56f14739a87c48413b0ce8de
jas20202 <99250573+jas20202@users.noreply.github.com>
2024-03-23 18:40:48 +0100

    settings actions

---
+++ PWManager.CLI/Enums/SettingsActions.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PWManager.CLI.Enums;
+public enum SettingsActions {
+    [Display(Name = "Change Main Group")]
+    MAIN_GROUP,
+    [Display(Name = "Change Clipboard Timeout")]
+    CLIPBOARD_TIMEOUT,
+    [Display(Name = "Change Password Criterias")]
+    PASSWORD_CRITERIA,
+    [Display(Name = "Go Back")]
+    RETURN
+}
